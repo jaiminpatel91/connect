9aec2dafe066784f232aed5a08fccb9344ad859f
Jaimin <jaiminpatel91@gmail.com>
2017-09-25 17:03:23 -0400

    multiple component added

---
+++ my-app/src/app/app.left_panel.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-panel',
+  templateUrl: './app.left_panel.component.html'
+})
+export class LeftPanelComponent {
+  title = 'Left Panel';
+}
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app.header.component';
+import { LeftPanelComponent } from './app.left_panel.component';
+import { FooterComponent } from './app.footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftPanelComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
