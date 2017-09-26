019db8f16efbdc7efdb0b27b9ace3bce26466e26
Jaimin <jaiminpatel91@gmail.com>
2017-09-26 12:20:43 -0400

    add profile and newsfeed page

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './app.header.component';
 import { LeftPanelComponent } from './app.left_panel.component';
+import { NewsFeedComponent } from './app.newsfeed.component';
+import { ProfileComponent } from './app.profile.component';
 import { FooterComponent } from './app.footer.component';
+
+const appRoutes: Routes = [
+  { path: 'newsfeed', component: NewsFeedComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LeftPanelComponent,
+    NewsFeedComponent,
+    ProfileComponent,
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ my-app/src/app/app.newsfeed.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newsfeed',
+  templateUrl: './app.newsfeed.component.html'
+})
+export class NewsFeedComponent {
+  title = 'news';
+}
